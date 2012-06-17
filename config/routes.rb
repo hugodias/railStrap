eea2a868b2a7f2ffebd02a27d4bb0e069597b272
Pete Forde <pete@unspace.ca>
2012-06-16 22:25:19 -0400

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 RailStrap::Application.routes.draw do
 
+  root :to => "users#home"
+
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
+  post "sessions" => "sessions#create", :as => ""
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#home"
   
-  match '/users/tests' => 'users#tests'
   resources :users
-  resources :sessions
-
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
