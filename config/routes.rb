f8dd6dbea7155449959fc40651cdf601eae94c0e
Pete Forde <pete@unspace.ca>
2012-06-18 15:41:18 -0400

    failed login now redirects to root, also fixed sessions_path bug in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ RailStrap::Application.routes.draw do
 
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
-  post "sessions" => "sessions#create", :as => ""
+  post "sessions" => "sessions#create"
   get "sign_up" => "users#new", :as => "sign_up"
   
   resources :users
