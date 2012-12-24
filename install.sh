daa6bf9eb993110247cc345ec7cefa47400bb6a5
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2012-12-24 16:36:30 -0200

    Install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo 'Welcome to RailsStrap v0.2 install'
+echo 
+echo '-- Press some key to continue --'
+read key
+
+echo 'Type your root password to procede (We will just update your bundle)'
+sudo bundle install && sudo bundle update
+
+echo '... Creating your database ...'
+rake db:create
+rake db:micrate
+
+echo '... Database created. Starting server ... '
+rails s
+
