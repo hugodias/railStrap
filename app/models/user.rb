737207a40cc987e2a9c97916a2afc2c327f72f7a
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2012-12-26 16:14:41 -0200

    Included username in user validation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ActiveRecord::Base
   
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
+  validates_presence_of :username
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates_uniqueness_of :username
   
   def self.authenticate(email, password)
       
