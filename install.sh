7b4e2e07487f8edd4d55a8c2796a34765969177a
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2012-12-26 15:19:02 -0200

    Included create admin seed

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo 'Welcome to RailsStrap v0.2 install'
 echo 
 echo '-- Press some key to continue --'
 read key
+echo
 
 echo '... Updating your bundle ...'
 bundle install
@@ -10,6 +11,9 @@ bundle install
 echo '... Creating your database ...'
 rake db:create
 rake db:migrate
+
+echo '... Creating default admin user ... '
+rake db:seed
 
 echo '... Database created. Starting server ... '
 rails s
